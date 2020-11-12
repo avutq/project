6513da0f062fa485c9e33fe5447c3735881092a2
Nenad <nenadmarinkovic@protonmail.com>
2020-11-12 21:55:26 +0100

    initial commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,8 +6,6 @@ const preferDefault = m => (m && m.default) || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Nenad/Web Development/project/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/Nenad/Web Development/project/src/pages/index.js"))),
-  "component---src-pages-professionals-js": hot(preferDefault(require("/Users/Nenad/Web Development/project/src/pages/professionals.js"))),
-  "component---src-pages-startup-js": hot(preferDefault(require("/Users/Nenad/Web Development/project/src/pages/startup.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/Nenad/Web Development/project/src/pages/index.js")))
 }
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,6 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
     },
   ],
 };
